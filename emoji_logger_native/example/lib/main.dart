5407e274968211859fa5443b7b5201dd1d09e16d
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-10-13 16:31:33 -0700

    changes to kotlin file

--- emoji_logger_native/example/lib/main.dart
+++ emoji_logger_native/example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:emoji_logger_native/emoji_logger_native.dart';
 
 void main() {
@@ -14,36 +11,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await EmojiLoggerNative.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,9 +19,22 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                EmojiLoggerNative.debug('this is debug');
+              }, 
+              child: Text('debug'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                EmojiLoggerNative.error('this is error');
+              }, 
+              child: Text('error'),
+            )
+          ],
+        )
       ),
     );
   }
